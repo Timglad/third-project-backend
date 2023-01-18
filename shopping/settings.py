0f92fbeb678d29100e3a50b8f16c1f5292f5062d
ran <ran@goz.com>
2023-01-18 09:03:47 +0200

    updated settings.py

--- shopping/settings.py
+++ shopping/settings.py
@@ -138,6 +138,8 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://localhost:8000",
 ]
