39a481bd5fe1e37623ee9a07d935fbe969556ac4
ran <ran@goz.com>
2023-02-09 11:06:48 +0200

    working version without authentication and with login

--- shopping/settings.py
+++ shopping/settings.py
@@ -90,24 +90,24 @@ WSGI_APPLICATION = 'shopping.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 #if 'RENDER' in os.environ:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'shopping_db',
-        'USER': 'shopping_db_user',
-        'PASSWORD': 'gbuNAzAccVPjyZtqPESeqj0ki93FPGOe',
-        # 'HOST': 'dpg-cf422dmn6mps0qnc92pg-a',
-        'HOST': 'dpg-cf422dmn6mps0qnc92pg-a.frankfurt-postgres.render.com',        
-        'PORT': '5432',
-    }
-}
-#els#
 # DATABASES = {
-#            'default': {
-#            'ENGINE': 'django.db.backends.sqlite3',
-#            'NAME': BASE_DIR / 'db.sqlite3',
-#        }
-#    }
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'shopping_db',
+#         'USER': 'shopping_db_user',
+#         'PASSWORD': 'gbuNAzAccVPjyZtqPESeqj0ki93FPGOe',
+#         # 'HOST': 'dpg-cf422dmn6mps0qnc92pg-a',
+#         'HOST': 'dpg-cf422dmn6mps0qnc92pg-a.frankfurt-postgres.render.com',        
+#         'PORT': '5432',
+#     }
+# }
+#els#
+DATABASES = {
+           'default': {
+           'ENGINE': 'django.db.backends.sqlite3',
+           'NAME': BASE_DIR / 'db.sqlite3',
+       }
+   }
 
 
 
@@ -183,7 +183,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.IsAuthenticated',
     ],
 }
 
